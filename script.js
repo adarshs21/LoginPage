f55a795239ea3d10dcffb084f313f063373b150d
adarshs21 <adarshswain21@gmail.com>
2024-09-23 00:02:05 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+function toggleP() {
+    const passwordInput = document.getElementById('password');
+    if (passwordInput.type === 'password') {
+        passwordInput.type = 'text'; 
+    } else {
+        passwordInput.type = 'password'; 
+    }
+}
